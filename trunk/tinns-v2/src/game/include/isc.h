d1ed274e0102569da8b3fac075a1146a33e1da89
Namikon <Namikon@ef8e7eb3-8d0b-0410-9eab-9562649fb893>
2006-12-14 22:03:15 +0000

    - Updated ISC definition file (v2) - Updated isc.h: Added enum for ISC connection stages - Added ISC Example.txt to show a few examples of an  "normal" datatransfer between game and infoserver
    git-svn-id: http://www.linux-addicted.net/repos/tinns@45 ef8e7eb3-8d0b-0410-9eab-9562649fb893

--- trunk/tinns-v2/src/game/include/isc.h
+++ trunk/tinns-v2/src/game/include/isc.h
@@ -33,31 +33,59 @@
 #ifndef ISC_H
 #define ISC_H
 
+#define ISC_VER 2
+
+// Part of mConStatus bitmask
+#define ISC_CONNECTED 1
+#define ISC_ADMIN 2
+
+
 class PISC
 {
 	private:
+        enum PISC_Status
+        {
+            ISC_NOTCONNECTED,
+            ISC_HANDSHAKE01,  // "Hello" msg sent, awaiting answer
+            ISC_HANDSHAKE02,  // PW sent, awaiting answer
+            ISC_GSDATA01,     // Servername sent, awaiting answer
+            ISC_GSDATA02,     // IP, Port & Patchlevel sent, awaiting blabla..
+            ISC_GSDATA03,     // Players / Staff onlinecounter sent, ...
+            ISC_GSDATA04,     // Minlevel to connect, minlevel to see Playercount, minlevel to see server sent,....
+            ISC_CLIENTIDENT,  // Awaiting client identification answer
+            ISC_ADMINLOGIN,   // Awaiting answer to Infoserver control login request
+            ISC_ICTRL_AUTOACC,// Infoserver control: Awaiting response to AutoAccount command
+            ISC_ICTRL_LOGLV,  // Infoserver control: Awaiting response to LoginLevel command
+            ISC_ICTRL_RELOAD, // Infoserver control: Awaiting response to Reload command
+            ISC_ICTRL_SHUTD,  // Infoserver control: Awaiting response to Shutdown command
+            ISC_ICTRL_HIDE,   // Infoserver control: Awaiting response to HidePeople command
+            ISC_IDLE          // Updateloop running
+        } mLinkStatus;
+
+        u16 mConStatus;     // Bitmask of Connection status
+
         int isc_method;
-        
+
         bool use_isc;
         char isc_pwd;
-        
+
         bool use_mysql;
         int m_server_id;
         std::time_t mysql_update_intervall;
-        std::time_t mysql_delayed_update_intervall; 
+        std::time_t mysql_delayed_update_intervall;
         std::time_t mysql_last_update_time;
         int mysql_last_client_count;
         std::time_t mysql_last_count_decrease_time;
-        
+
         void Start_isc();
         void Update_isc();
         void Shutdown_isc();
-              
+
         void Start_mysql();
         void Update_mysql();
         bool do_mysql_db_update(int players, bool shutdown = false);
         void Shutdown_mysql();
-        
+
 	public:
 		PISC();
 		~PISC();
